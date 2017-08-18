a120338a546e02180459123f17a372ecadb33b23
yangkai <yangkaiwj49470@touna.cn>
2017-08-18 18:33:49 +0800

    增加Hystrix断路例子

--- springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/EurekaServer.java
+++ springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/EurekaServer.java
@@ -2,10 +2,12 @@ package com.touna.study.studyspringcloudeureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCircuitBreaker
 public class EurekaServer {
 
 	public static void main(String[] args) {
--- springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/controller/ServerTestController.java
+++ springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/controller/ServerTestController.java
@@ -3,11 +3,18 @@ package com.touna.study.studyspringcloudeureka.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/server")
 public class ServerTestController {
 	@RequestMapping("/say")
+	@HystrixCommand(fallbackMethod="fallback")
 	public String server(){
+		int b = 2/0;
 		return "eureka server";
+	}
+	public String fallback(){
+		return "wrong fall back";
 	}
 }

a74fa661f2d3bea22d94342a4856bb620e72533d
yangkai <yangkaiwj49470@touna.cn>
2017-08-18 18:40:36 +0800

    添加hystrix仪表盘

--- springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/EurekaServer.java
+++ springcloudeureka/src/main/java/com/touna/study/studyspringcloudeureka/EurekaServer.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class EurekaServer {
 
 	public static void main(String[] args) {
